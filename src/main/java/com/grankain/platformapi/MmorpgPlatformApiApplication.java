6677b571e974c8374ef3e13e015dfaf943ed9296
Ewerton Campos <ewertoncampos@gmail.com>
2026-04-29 08:17:25 -0300

    Start Project

---
+++ src/main/java/com/grankain/platformapi/MmorpgPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.grankain.platformapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmorpgPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmorpgPlatformApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grankain/platformapi/MmorpgPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grankain.platformapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmorpgPlatformApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
